0dd565b22677aae84065b32bb47d445f9fb4829b
QianlingC <chowqianlingg@gmail.com>
2022-11-01 00:12:53 +0800

    second-commit

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,14 @@ import bggradient from "./assets/images/gradient-layer-2.png";
 import bgimage from "./assets/images/image-3.jpg";
 import bluecurl from "./assets/images/shape-17.png";
 import boy_study from "./assets/images/content-image-6.png";
+import dots from "./assets/images/pattern-1.png";
+import lpattern from "./assets/images/pattern-35.png";
+import smile from "./assets/images/about-6 (1).png";
+import holdhands from "./assets/images/about-9.png";
+import pencil from "./assets/images/about-7.png";
+import together from "./assets/images/about-8.png";
+import money from "./assets/images/about-6.png";
+
 import Navbar from "./components/Navbar";
 
 class Home extends Component {
@@ -34,7 +42,50 @@ class Home extends Component {
           </div>
         </div>
 
-        <div className="home-center"></div>
+        <section className="home-center">
+          <div className="image-stats">
+            <img src={dots} className="dots" alt="dots" />
+            <img src={lpattern} className="bigcurve" alt="pattern" />
+            <img src={smile} className="smile" alt="people-smile" />
+            <div className="circle-text">
+              <h3>45K</h3>
+              <div className="line"></div>
+              <p>Satisfied Donors</p>
+            </div>
+          </div>
+          <div className="about">
+            <p style={{ color: "orange" }}>ABOUT AELPA</p>
+            <h1>Aelpa Make an education charity donation today</h1>
+            <p>
+              Growing up in poverty, children face many challenges: hunger and
+              health problems, limited access to education and medical
+              services.Growing up in poverty children face many
+            </p>
+            <br></br>
+            <hr></hr>
+
+            <br></br>
+            <img src={pencil} className="pencil-bullet" alt="pencil" />
+            <span>20,000 poor children education and empowered</span>
+            <br></br>
+            <br></br>
+            <img src={together} className="together-bullet" alt="together" />
+            <span>42,000 children lifted from poverty</span>
+            <br></br>
+            <br></br>
+            <img src={holdhands} className="hold-bullet" alt="holdhands" />
+            <span>9,000 children healthy and nourished</span>
+            <br></br>
+            <br></br>
+            <hr></hr>
+            <br></br>
+            <img src={money} className="money-bullet" alt="money" />
+            <span>
+              Make an education charity donation today and give a child the
+              opportunity for a brighter future.
+            </span>
+          </div>
+        </section>
       </>
     );
   }
